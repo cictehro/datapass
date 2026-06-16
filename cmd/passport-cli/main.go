16b8b172065fcf6ca2cd5ecca80d483ecf7880be
cictehro <145049253+cictehro@users.noreply.github.com>
2026-06-16 19:10:25 +0300

    Update main.go

--- cmd/passport-cli/main.go
+++ cmd/passport-cli/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 "fmt"
+"os"
 )
 
 func main() {
-fmt.Println("Passport Index CLI")
+if len(os.Args) < 2 {
+fmt.Println("passport-cli")
+fmt.Println("commands: lookup, rank")
+return
+}
+
+```
+switch os.Args[1] {
+case "lookup":
+	fmt.Println("lookup not implemented")
+case "rank":
+	fmt.Println("rank not implemented")
+default:
+	fmt.Println("unknown command")
+}
+```
+
 }
